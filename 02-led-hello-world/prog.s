47eeebcbc33de81270e49612cc8414f2b60f4530
wilypomegranate <wilypomegranate@users.noreply.github.com>
2020-04-04 16:43:12 -0400

    Clear and set specific GPIO_CRH bits.

--- 02-led-hello-world/prog.s
+++ 02-led-hello-world/prog.s
@@ -14,8 +14,15 @@ ldr r0, [r1] @ Load the current value of RCC_APB2ENR into r0.
 orr r0, #0x00000010 @ Set bit to enable GPIOC clock.
 str r0, [r1] @ Write new value to RCC_APB2ENR.
 
-ldr r0, =0x44544444 @ Set the specific bits to enable 10MHZ output on GPIOC.
-ldr r1, =0x40011004 @ Location of GPIOC register.
+@ldr r0, =0x44544444 @ Set the specific bits to enable 10MHZ output on GPIOC.
+ldr r1, =0x40011004 @ Location of GPIOC_CRH register.
+ldr r0, [r1] @ Load the current value of GPIOC_CRH into r0.
+@ Set PC13 as push pull output. This is set via CNF13.
+@ Bits 22,23 to 00.
+@ Set PC13 as 10Mhz output. This is set via MODE13.
+@ Bits 20,21 to 01.
+and r0, #0xff2fffff
+orr r0, #0x200000
 str r0, [r1]
 
 ldr r0, =0x2000
